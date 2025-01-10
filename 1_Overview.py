7f5ad744b11f01e7c21f3676e322489c877c4b98
MinhHung7 <hungm0434@gmail.com>
2025-01-10 15:03:23 +0700

    add openai version

--- 1_Overview.py
+++ 1_Overview.py
@@ -340,7 +340,7 @@ with st.container(border=False):
             y=accidents_by_day_vn.values,
             labels={'x': 'Thứ trong tuần', 'y': 'Số vụ tai nạn'},
             title='📊 Số vụ tai nạn theo các thứ trong tuần',
-            # text=accidents_by_day_vn.values,
+            text=accidents_by_day_vn.values,
             # text='Số vụ tai nạn',
             # color='Số vụ tai nạn',
             color=accidents_by_day_vn.values,
@@ -378,8 +378,8 @@ with st.container(border=False):
             font=dict(size=12),
             height=300,
             
-            legend_title=dict(
-                text='Số vụ'
+            coloraxis_colorbar=dict(
+                title='Số vụ'  # Thay đổi tiêu đề cho thanh màu (legend)
             )
         )
 
